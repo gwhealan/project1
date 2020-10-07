cc7099d338902e2a2007a7679900050a2ffcbf50
Antonio Silva Paucar <antonios@uoreogon.eedu>
2020-10-07 12:55:51 -0700

    gitignore

--- distance.py
+++ distance.py
@@ -6,6 +6,6 @@ def distance(ini_lat, ini_lon, fin_lat, fin_lon):
 	dif_lon = radians(ini_lon) - radians(fin_lon)
 	earth_radio = 6371 #radius in km
 	distance = (sin(dif_lat/2))**2 + cos(radians(ini_lat)) * cos(radians(fin_lat)) * (sin(dif_lon/2))**2
-	distancia = earth_radio * asin(sqrt(distance)) * 2 * 1000 # multiply by 1000 to convert to distance in meters
+	distancia = earth_radio * asin(sqrt(distance)) * 2 * 1000 # multiply by 1000 to convert to 	distance in meters
 
 	return distancia
